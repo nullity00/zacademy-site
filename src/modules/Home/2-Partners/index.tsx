893ade156c7f2d04bb95f9c3f6c12e46eeba6d92
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-03-22 18:16:05 +0530

    our clients responsive

--- src/modules/Home/2-Partners/index.tsx
+++ src/modules/Home/2-Partners/index.tsx
@@ -15,9 +15,9 @@ const ClientSection: React.FC = () => {
   ];
 
   return (
-    <section className="bg-white py-8">
+    <section className="bg-white my-32 sm:my-16">
       <div className="container mx-auto">
-        <h2 className="text-5xl font-bold mb-10 text-left">Our Clients</h2>
+        <h2 className="text-5xl sm:text-3xl font-bold mb-10 text-left">Our Clients</h2>
         <div className="grid grid-cols-3 md:grid-cols-3 gap-4">
           {clients.map((client, index) => (
             <div
@@ -28,10 +28,10 @@ const ClientSection: React.FC = () => {
               <img
                 src={client.logo}
                 alt={`${client.name} Logo`}
-                className="mx-left ml-4 mt-4 mb-4 w-12 h-12"
+                className="mx-left sm:mx-auto ml-4 mt-4 mb-4 w-12 h-12 sm:w-8 sm:h-8"
               />
               {/* Client Name */}
-              <p className="text-gray-800 text-left font-bold text-lg ml-4 mb-4">{client.name}</p>
+              <p className="text-gray-800 text-left sm:text-center font-bold text-lg sm:text-xs ml-4 sm:ml-0 mb-4">{client.name}</p>
               {/* Client Website Link */}
               <a
                 href={client.website}
