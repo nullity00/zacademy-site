8cf0cac7d866b5a1a32b82f1e9750de9ad9b324f
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-03-22 18:21:25 +0530

    home research responsive

--- src/modules/Home/3-Sections/Research.tsx
+++ src/modules/Home/3-Sections/Research.tsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 export default function Research() {
   return (
     <>
-      <div className="grid grid-cols-2 gap-4 my-32">
-        <div className="col-span-1">
+      <div className="grid grid-cols-2 gap-4 my-32 sm:my-16 sm:mx-6">
+        <div className="col-span-1 sm:hidden">
           <img src="/code.svg" className="h-[15rem] mt-8" />
         </div>
-        <div className="col-span-1 mr-10">
+        <div className="col-span-1 sm:col-span-3 mr-10 sm:mr-0">
           <h1 className="lg:text-5xl font-bold lg:leading-normal">
             {" "}
             Shh.. it's a Secret: <br /> ZK Research
           </h1>
-          <p className="text-zinc-500 mb-8">
+          <p className="text-zinc-500 mb-8 sm:text-justify">
             <br />
             In the realm of Innovation, we take pride in our forward-thinking
             approach, evident in our incubation of novel programs such as the ZK
@@ -25,6 +25,9 @@ export default function Research() {
               Read Our Blogs
             </button>
           </Link>
+        </div>
+        <div className="sm:col-span-3 md:hidden lg:hidden">
+          <img src="/code.svg" className="h-[15rem] mt-8" />
         </div>
       </div>
     </>
