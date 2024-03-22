dd61602b19cfe3542d8fde4e0f75ef76ae5c3d24
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-03-22 18:23:07 +0530

    members-page

--- src/pages/Members.tsx
+++ src/pages/Members.tsx
@@ -1,11 +1,13 @@
 import Footer from "../modules/Footer";
 import Core from "../modules/Members/Core";
+import Residents from "../modules/Members/Residents"
 
 export default function MembersPage() {
   return (
     <>
-      <div className="mx-40 h-full relative">
+      <div className="mx-40 sm:mx-10 h-full relative">
         <Core />
+        <Residents />
       </div>
       <Footer />
     </>
