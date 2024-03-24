dfe3f25ee2529cc9f404b9a3cc1ad33206c1aab4
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-03-24 18:14:58 +0530

    Residents

--- src/modules/Members/Residents/index.tsx
+++ src/modules/Members/Residents/index.tsx
@@ -9,26 +9,26 @@ const resident_members = [
     { name: "adriro", twitter: "https://twitter.com/adrianromero" },
     { name: "ljmanini", twitter: "https://twitter.com/ljmanini" },
     { name: "popular", twitter: "https://twitter.com/popular_12345/" },
-  ];
+];
   
-  export default function Core() {
+export default function Residents() {
     return (
       <div className="my-16">
         <h1 className="text-4xl mb-8">Residents</h1>
         <ul
           role="list"
-          className="grid grid-cols-3 md:grid-cols-3 gap-4 gap-y-8"
+          className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 gap-4 gap-y-8"
         >
           {resident_members.map((person) => (
             <li key={person.name}>
-              <div className="flex flex-row sm:grid items-center gap-x-6">
+              <div className="flex flex-row sm:flex-col items-center gap-x-6">
                 <img
-                  className="h-11 w-13 rounded-full"
+                  className="h-11 w-11 sm:h-9 sm:w-9 sm:mb-2 rounded-full items-center"
                   src={Math.floor(Math.random()*14).toString()+".png"}
                   alt="Core"
                 />
                 <div>
-                  <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900 hover:underline">
+                  <h3 className="text-base sm:text-xs font-semibold leading-7 tracking-tight text-gray-900 hover:underline">
                     <a href={person.twitter}>{person.name}</a>
                   </h3>
                   {/* <p className="text-sm font-semibold leading-6 text-indigo-600">
@@ -41,5 +41,5 @@ const resident_members = [
         </ul>
       </div>
     );
-  }
+}
   
