7fd010ff09f283b5c181ba6fff2041b4fd3bba6f
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-03-24 18:12:51 +0530

    Core

--- src/modules/Members/Core/index.tsx
+++ src/modules/Members/Core/index.tsx
@@ -11,18 +11,18 @@ export default function Core() {
       <h1 className="text-4xl mb-8">Core</h1>
       <ul
         role="list"
-        className="grid grid-cols-3 md:grid-cols-3 gap-4 gap-y-8"
+        className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 gap-4 gap-y-8"
       >
         {core_members.map((person) => (
           <li key={person.name}>
-            <div className="flex flex-row sm:grid items-center gap-x-6">
+            <div className="flex flex-row sm:flex-col items-center gap-x-6">
               <img
-                className="h-11 w-11 rounded-full"
+                className="h-11 w-11 sm:h-9 sm:w-9 sm:mb-2 rounded-full items-center"
                 src={Math.floor(Math.random()*14).toString()+".png"}
                 alt="Core"
               />
               <div>
-                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900 hover:underline">
+                <h3 className="text-base sm:text-xs font-semibold leading-7 tracking-tight text-gray-900 hover:underline">
                   <a href={person.twitter}>{person.name}</a>
                 </h3>
                 {/* <p className="text-sm font-semibold leading-6 text-indigo-600">
