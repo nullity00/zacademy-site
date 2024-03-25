6411f2f9075cc047b84b3a06343c7729c15304be
nullity00 <reshmashaik3011@gmail.com>
2024-03-25 06:39:16 +0530

    contact page

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,13 @@
+import ContactUs from "../modules/Contact";
+import Footer from "../modules/Footer";
+
+export default function ContactPage() {
+  return (
+    <>
+      <div className="lg:mx-40 mx-6 h-full relative">
+        <ContactUs />
+      </div>
+      <Footer />
+    </>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import HeroSection from "../modules/Home/1-Hero-Section";
 import Sections from "../modules/Home/3-Sections";
-import PartnerUp from "../modules/Home/4-Contact-Form";
+import PartnerUp from "../modules/Home/4-Contact-Us";
 import Footer from "../modules/Footer";
 import Partners from "../modules/Home/2-Clients";
 
