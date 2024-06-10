cdb6762cb0ab746fc16c1bddda3c8ac4357c6ed3
nullity00 <reshmashaik3011@gmail.com>
2024-06-10 16:28:10 +0530

    zblock1

---
+++ src/modules/Zblock1/Data/zblock1.ts
@@ -0,0 +1,157 @@
+export const m1video = [
+  {
+    id: "xMfrMdfIMP8",
+    title: "Karp reduction I",
+    description: "Exact Cover, Knapsack",
+    link: "https://docs.google.com/presentation/d/1rZVI6c9Wo-kU9xKINOMncWywvQHn0952EsMayviHwYM/edit?usp=sharing",
+  },
+  {
+    id: "jDTRoFfdMio",
+    title: "Karp reduction II",
+    description: "SAT, 3COL, Witnesses",
+    link: "https://docs.google.com/presentation/d/1QeC4c3-xxEx27wX4Q-iWcowCdUws1lpufLIjjvlN_BE/edit?usp=sharing",
+  },
+  {
+    id: "PI_G3IASdiw",
+    title: "Gödel reduction",
+    description: "provability",
+    link: "https://docs.google.com/presentation/d/1IerLvER_WEEJIswZB25ACb531D9QrctB-F_FYFNG1ag/edit?usp=sharing",
+  },
+  {
+    id: "A4Kx0Hd-u2w",
+    title: "Turing reduction",
+    description: "universality, decidability",
+    link: "https://docs.google.com/presentation/d/1HWUPQRfwnBYnUHACSud3Bsx8KSFm1C8K/edit?usp=sharing&ouid=115947387247847572737&rtpof=true&sd=true",
+  },
+  {
+    id: "1wJx3VizBww",
+    title: "Cook reduction",
+    description: "NP in ZK",
+    link: "https://docs.google.com/presentation/d/1RwEMf4GEepPiOo4B-t27bsf9LgS-JO9zfsw9l3-mO1o/edit?usp=sharing",
+  },
+];
+
+export const m1exercises = [
+  {
+    title: "Karp Reduction",
+    link: "https://sweltering-bill-f01.notion.site/Module-1-Karp-reduction-exercise-d4176a28430240a59f60ca7c00ac021c",
+  },
+  {
+    title: "Circom Quiz and Puzzle",
+    link: "https://sweltering-bill-f01.notion.site/Module-1-Circom-Quiz-Puzzle-a526f578861644798108ba0ee7573f77",
+  },
+];
+
+export const m1notes = {
+  title: "Circom startup kit",
+  link: "https://docs.google.com/presentation/d/1ebxaq9fsBJxOEpEnqaj9vYloNQT82DkAgp2d5JJRI_k/edit?usp=sharing",
+};
+
+export const m1discussion = {
+  title: "module-1-zk-context",
+  link: "https://discord.gg/5vVJ7hY",
+};
+
+export const m2video = [
+  {
+    id: "nurBnWQPsCY",
+    title: "Codebase overview",
+    description: "",
+    link: "https://docs.google.com/presentation/d/1sQ9vLqSIxe8zNChWxq2jEPv3gtJCn-dtew0m3HOOJ1g/edit#slide=id.p",
+  },
+  {
+    id: "Y24bhP7QVCA",
+    title: "Office hour",
+    description: "",
+    link: "https://docs.google.com/document/d/1WBOeAGRjZQy-6rm_DzzvRmSweYEcsKhXs5hYzOx11lw/edit",
+  },
+  {
+    id: "FcLvM-bb3Ds",
+    title: "Audit report & review",
+    description: "",
+    link: "https://github.com/zBlock-1/RLN-audit-report",
+  },
+];
+
+export const m2guestLectures = [
+  {
+    id: "h_ie8eaPNcM",
+    title: "Pairing with Lev Soukhanov (Ethereum Foundation)",
+    link: "https://docs.google.com/presentation/d/1BsA9RhgxBY8H4E8uOvesLptBIL11MaILOEyipfjSXso/edit#slide=id.p",
+    description: "",
+  },
+  {
+    id: "Hz_XHfxunck",
+    title: "Groth16 with Yin Tong (Geometry)",
+    link: "https://docs.google.com/presentation/d/1vYUiNYv8PRI2SVEKC3uz54I475d79E9XxvCWwkC7kPQ/edit#slide=id.p",
+    description: "",
+  },
+];
+
+export const m2discussion = {
+  title: "module-2-rln-audit",
+  link: "https://discord.gg/Y8ccpfcYmq",
+};
+
+export const m2report = {
+  title: "RLN Audit Reports",
+  link: "https://github.com/zBlock-1/RLN-audit-report",
+};
+
+export const m3video = [
+  {
+    id: "jkzfspE1Bng",
+    title: "Codebase overview",
+    description: "Part I",
+    link: "https://docs.google.com/presentation/d/1CFGsEU6moOBCTIvXU0Yxg62aD5fZ-d5srf1o5ZaUJIo/edit?usp=sharing",
+  },
+  {
+    id: "JRANJdUraoM",
+    title: "Codebase overview",
+    description: "Part II",
+    link: "https://drive.google.com/drive/u/0/folders/1VmrcYdf0e_7JTB1Zlqo6ekg3H08rlbxL",
+  },
+  {
+    id: "XXLxkR-QrZk",
+    title: "Office hour 1",
+    description: "",
+    link: "https://github.com/zBlock-1/RLN-audit-report",
+  },
+  {
+    id: "G0d3BgFCTxM",
+    title: "Office hour 2",
+    description: "",
+    link: "https://docs.google.com/document/d/1vre71efNxszfmU36j-ExQhOuATDCeuzd4Zuht5DKBoY/edit",
+  },
+  {
+    id: "7D1hV_2G8G0",
+    title: "Audit reports & review",
+    description: "",
+    link: "https://github.com/zBlock-1/spartan-ecdsa-audit-report",
+  },
+];
+
+export const m3exercises = [
+  {
+    title: "Quiz 1",
+    link: "https://hackmd.io/68TlQZFvQOeMZ2mvFl0Abg?view#Session-11",
+  },
+  {
+    title: "Quiz 2",
+    link: "https://hackmd.io/68TlQZFvQOeMZ2mvFl0Abg?view#Session-21",
+  },
+];
+
+export const m3guestLectures = [
+  {
+    id: "-SXMd6S0r0I",
+    title: "PLONKish Arithmetization with Pratyush R Tiwari",
+    description: "",
+    link: "https://drive.google.com/drive/u/0/folders/1nDEbHcXO6ttPtyRq46Pe36C3wmtxgLTF",
+  },
+];
+
+export const m3report = {
+  title: "Spartan-ECDSA Audit Report",
+  link: "https://github.com/zBlock-1/spartan-ecdsa-audit-report",
+};
--- src/pages/Zblock.tsx
+++ src/pages/Zblock.tsx
@@ -1,23 +1,15 @@
 // src/App.tsx
-import React from 'react';
-import Module1 from '../modules/Zblock1/Module1';
-import Module2 from '../modules/Zblock1/Module2';
-import Module3 from '../modules/Zblock1/Module3';
-import Footer from '../modules/Footer';
-
-
+import React from "react";
+import ZBlock1Modules from "../modules/Zblock1/zBlock1";
+import Footer from "../modules/Footer";
 
 export default function ZblockPage() {
   return (
     <>
-      <div className="p-8">
-        <Module1 />
-        <Module2 />
-        <Module3 />
+      <div className="p-8 my-16 lg:mx-32">
+        <ZBlock1Modules />
       </div>
       <Footer />
     </>
   );
-};
-
-
+}
