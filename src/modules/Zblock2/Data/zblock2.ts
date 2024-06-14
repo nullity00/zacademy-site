e747b44551255f32ab6de3897b4fa969b818d2e8
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-06-14 14:14:56 +0530

    data-zblock2

---
+++ src/modules/Zblock2/Data/zblock2.ts
@@ -0,0 +1,172 @@
+export const m1video = [
+    {
+      id: "EUI2L1RhtGI",
+      title: "yAcademy Opening Session",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1-wmMXStg__5IPzs1MDWN1uxOa36aFkRQ?usp=drive_link",
+    },
+    {
+      id: "TAVTzLSI-gU",
+      title: "Summa | Proof of Solvency",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1SSzHwvOgab5FnlgmEjoyKuNiNYlh3Lgw?usp=drive_link",
+    },
+    {
+      id: "9iCJ6GqHpo4",
+      title: "Summa | Protocol overview",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1N3vDwrGifY_tbKbhKvTB22oRaAUTd7vo?usp=drive_link",
+    },
+    {
+      id: "KpGlTC7fNVk",
+      title: "Summa | Protocol Intro to Summa",
+      description: "",
+      link: "https://drive.google.com/drive/folders/13HYik8qTA4uYvYBed8iFPjhojS_eCVRJ?usp=drive_link",
+    },
+    {
+      id: "0ZfUKTc_qoE",
+      title: "Summa | Protocol Codebase Overview 1",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1XnXCzF9X9_zz8RidKy24-QuVvLqYi-4p?usp=drive_link",
+    },
+    {
+      id: "8S3SRg0DPlk",
+      title: "Summa | Protocol Codebase Overview 1",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1RehDHoexuhUrzjDqGfDFAZTiyBDN2gfR?usp=drive_link",
+    },
+    {
+      id: "p6wGvJ4VPck",
+      title: "Summa Protocol | Codebase Overview 2",
+      description: "",
+      link: "https://drive.google.com/drive/folders/13IVDe0YMt72imiLJSv1l4wavzKR1beBs?usp=drive_link",
+    },
+    {
+      id: "cjed5gnrj_I",
+      title: "Summa Protocol | Smart Contract Overview",
+      description: "",
+      link: "https://drive.google.com/drive/folders/15MbHe6C_WhZenre933Rl2Z38Ce9idh7_?usp=drive_link",
+    },
+    {
+      id: "QvJyQeCdmaU",
+      title: "Taxonomy of ZK Vulnerabilities by rkm0959",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1B1h-Y3wRYZhfGKJJdxDthBxE7uQQtbgn?usp=drive_link",
+    },
+    {
+      id: "jSEvIHmAke0",
+      title: "yAcademy | Office Hours",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1R2uQ6M7AeUH8VvLOJRTUVZUpkY1Y6x9a?usp=drive_link",
+    },
+    {
+      id: "9RTjdn8NElk",
+      title: "yAcademy Resident talk | Proof of Solvency by Nagu",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1T438y7TJa4e7BJSdCWdM5QvkbZ9Z8zxa?usp=drive_link",
+    },
+    {
+      id: "6uDpGraAeEQ",
+      title: "yAcademy | Office Hours II",
+      description: "",
+      link: "https://drive.google.com/drive/folders/19U-xYiJ4NZL95DPQmzX2Ysjql86w1RUs?usp=drive_link",
+    },
+  ];
+  
+  export const m1exercises = [
+    {
+      title: "Karp Reduction",
+      link: "https://sweltering-bill-f01.notion.site/Module-1-Karp-reduction-exercise-d4176a28430240a59f60ca7c00ac021c",
+    },
+    {
+      title: "Circom Quiz and Puzzle",
+      link: "https://sweltering-bill-f01.notion.site/Module-1-Circom-Quiz-Puzzle-a526f578861644798108ba0ee7573f77",
+    },
+  ];
+  
+  export const m1notes = {
+    title: "Circom startup kit",
+    link: "https://docs.google.com/presentation/d/1ebxaq9fsBJxOEpEnqaj9vYloNQT82DkAgp2d5JJRI_k/edit?usp=sharing",
+  };
+  
+  export const m1discussion = {
+    title: "module-1-zk-context",
+    link: "https://discord.gg/5vVJ7hY",
+  };
+  
+  export const m2video = [
+    {
+      id: "2bEiNcfkxBQ",
+      title: "Guest Speaker | KZG",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1aD0u0fPyRkmZFya3sq80n0WCWi66RByX?usp=drive_link",
+    },
+    {
+      id: "yZ9NJ9x75iY",
+      title: "Guest Speaker | Axiom V2",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1iQVFMVLLD5VqQjHUZiRurwzvBrV7E2sY?usp=drive_link",
+    },
+    {
+      id: "KLq3dwlu2xw",
+      title: "Guest Speaker | PowdrVM",
+      description: "",
+      link: "https://drive.google.com/drive/folders/15BMnPT3rf5mmUdD8qCCtQ4fP98AaOuU2?usp=drive_link",
+    },
+    {
+      id: "c3yQigJO3kc",
+      title: "Mathematical Foundations of the Polynomial Interpolation Approach",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1081DlyvigZykFaWmI3JCSknpnUEw0z67?usp=drive_link",
+    },
+    {
+      id: "37lTuP20jbE",
+      title: "Univariate Grand Sum Circuit Overview",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1k-8HDie_adueyjY0-TIhHaLawtT6p27d?usp=drive_link",
+    },
+    {
+      id: "o5H81gi6n-o",
+      title: "Summa | V2 Smart Contract Overview",
+      description: "",
+      link: "https://drive.google.com/drive/folders/187RrwO4cVWJyS-lqjWVWC0Iso9gdtxq5?usp=drive_link",
+    },
+    {
+      id: "nWM6oHo1-Ek",
+      title: "Summa | Vb Prover-side Code Walkthrough",
+      description: "",
+      link: "https://drive.google.com/drive/folders/1EXA-JiOYQWIioK8UpkmyG_9e-I6zXiOj?usp=drive_link",
+    },
+    {
+      id: "3ie2yZ7CT5A",
+      title: "Lookup Arguments in Halo2 by Ying Tong",
+      description: "",
+      link: "https://drive.google.com/drive/folders/18DvFxBapDtksfDGePirIJmQcicmMTwva?usp=drive_link",
+    },
+    
+  ];
+  
+  export const m2guestLectures = [
+    {
+      id: "h_ie8eaPNcM",
+      title: "Pairing with Lev Soukhanov (Ethereum Foundation)",
+      link: "https://docs.google.com/presentation/d/1BsA9RhgxBY8H4E8uOvesLptBIL11MaILOEyipfjSXso/edit#slide=id.p",
+      description: "",
+    },
+    {
+      id: "Hz_XHfxunck",
+      title: "Groth16 with Yin Tong (Geometry)",
+      link: "https://docs.google.com/presentation/d/1vYUiNYv8PRI2SVEKC3uz54I475d79E9XxvCWwkC7kPQ/edit#slide=id.p",
+      description: "",
+    },
+  ];
+  
+  export const m2discussion = {
+    title: "module-2-rln-audit",
+    link: "https://discord.gg/Y8ccpfcYmq",
+  };
+  
+  export const m2report = {
+    title: "RLN Audit Reports",
+    link: "https://github.com/zBlock-1/RLN-audit-report",
+  };
