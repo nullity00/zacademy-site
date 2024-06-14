4714d3958e4d2a556de597adce758802a0ddd330
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-06-14 14:13:26 +0530

    app.tsx - zblock2

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Navbar, MobileNavbar } from "./modules/Navbar";
 import MembersPage from "./pages/Members";
 import ContactPage from "./pages/Contact";
 import ZblockPage from "./pages/Zblock";
+import Zbloc2kPage from "./pages/Zblock2";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -24,6 +25,7 @@ function App() {
           <Route path="/members" element={<MembersPage />} />
           <Route path="/contact-us" element={<ContactPage />} />
           <Route path="/zBlock1" element={<ZblockPage />} />
+          <Route path="/zBlock2" element={<Zbloc2kPage />} />
         </Routes>
       </div>
     </Router>
