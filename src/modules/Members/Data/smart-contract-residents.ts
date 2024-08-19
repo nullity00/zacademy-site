1d9e0fcc11b19b869119ee8bdbd1f7a822494a94
nullity00 <reshmashaik3011@gmail.com>
2024-08-19 15:04:32 +0530

    rename

--- src/modules/Members/Data/smart-contract-residents.ts
+++ src/modules/Members/Data/smart-contract-residents.ts
@@ -1,12 +1,15 @@
-export const residents = [
+export const sc_residents = [
   { name: "engn33r", twitter: "https://twitter.com/bl4ckb1rd71" },
   { name: "Nibbler Express", twitter: "" },
   { name: "sjkelleyjr", twitter: "https://twitter.com/sjkelleyjr" },
+  { name: "devtooligan", twitter: "https://twitter.com/devtooligan" },
   { name: "prady", twitter: "https://twitter.com/prady_v" },
   { name: "panda", twitter: "https://twitter.com/pandadfi" },
   { name: "spalen", twitter: "https://github.com/spalen0" },
   { name: "HHK", twitter: "https://twitter.com/HHK_ETH" },
   { name: "adriro", twitter: "https://twitter.com/adrianromero" },
-  { name: "ljmanini", twitter: "https://twitter.com/ljmanini" },
   { name: "popular", twitter: "https://twitter.com/popular_12345/" },
+  { name: "_eperezok", twitter: "https://twitter.com/_eperezok" },
+  { name: "fedebianu", twitter: "https://twitter.com/fedebianu" },
+  { name: "DrasticWatermelon", twitter: "" },
 ];
