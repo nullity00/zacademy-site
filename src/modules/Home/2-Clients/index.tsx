4537064dac1a1029b6bcda7b6b614d0fd8d5c3ca
nullity00 <reshmashaik3011@gmail.com>
2024-12-15 03:03:26 +0400

    clients

--- src/modules/Home/2-Clients/index.tsx
+++ src/modules/Home/2-Clients/index.tsx
@@ -1,41 +1,66 @@
 import OrbitingCircles from "../../../components/ui/orbiting-circles";
 
+const clients = [
+  { name: "Exit10", logo: "exit10.png", website: "https://exit10.com/" },
+  { name: "Vmex", logo: "vmex.png", website: "https://vmex.finance/" },
+  {
+    name: "Timeless Finance", logo: "timelessfi.svg", website: "https://timelessfi.com/",
+  },
+  { name: "Manifold Finance", logo: "manifold.svg", website: "https://manifoldfinance.com/" },
+  { name: "Rage Trade", logo: "rage.svg", website: "https://ragetrade.io/" },
+  { name: "Strike", logo: "strike.svg", website: "https://www.strike.org/" },
+  { name: "Inverse", logo: "inverse.svg", website: "https://inverse.finance/" },
+  { name: "Ramses", logo: "ramses.svg", website: "https://ramses.exchange/" },
+  { name: "vfat", logo: "vfat.svg", website: "https://vfat.io/" },
+  { name: "TLX", logo: "tlx.svg", website: "https://tlx.fi/" },
+  { name: "Peapods", logo: "peapods.svg", website: "https://peapods.finance/" },
+  { name: "Cove", logo: "cove.svg", website: "https://cove.finance/" },
+  { name: "Rysk", logo: "rysk.svg", website: "https://www.rysk.finance/" },
+  {
+    name: "Superfluid",
+    logo: "superfluid.svg",
+    website: "https://www.superfluid.finance/",
+  },
+  {
+    name: "Get Protocol",
+    logo: "get-protocol.svg",
+    website: "https://www.get-protocol.io/",
+  },
+  {
+    name: "Llama Pay",
+    logo: "llamapay.svg",
+    website: "https://llamapay.io/",
+  },
+  {
+    name: "Lamalend",
+    logo: "llamalend.svg",
+    website: "https://llamalendnft.com/",
+  },
+  { name: "Temple", logo: "templedao.svg", website: "https://templedao.link/" },
+
+  { name: "Curve", logo: "curve.svg", website: "https://curve.fi/" },
+  { name: "Euler", logo: "euler.svg", website: "https://www.euler.finance/" },
+  { name: "Asymmetry", logo: "asymetry.svg", website: "https://www.asymmetry.finance/" },
+
+  {
+    name: "Personae Lab",
+    logo: "personaelab.png",
+    website: "https://www.personaelabs.xyz/",
+  },
+  {
+    name: "Privacy & Scaling Explorations",
+    logo: "pse.png",
+    website: "https://pse.dev/en",
+  },
+  { name: "Olumpus DAO", logo: "olympus.svg", website: "https://www.olympusdao.finance/" },
+  {
+    name: "Nouns DAO", logo: "nouns.svg", website: "https://nouns.wtf/",
+  },
+  { name: "Yearn", logo: "yearn.svg", website: "https://yearn.fi/" },
+];
+
 export default function ClientSection() {
-  const clients = [
-    { name: "Yearn", logo: "yearn.png", website: "https://yearn.fi/" },
-    {
-      name: "Superfluid",
-      logo: "superfluid.png",
-      website: "https://www.superfluid.finance/",
-    },
-    {
-      name: "Get Protocol",
-      logo: "getprotocol.png",
-      website: "https://www.get-protocol.io/",
-    },
-    {
-      name: "Llama Pay",
-      logo: "llamapay.png",
-      website: "https://llamapay.io/",
-    },
-    {
-      name: "Lamalend",
-      logo: "lamalend.png",
-      website: "https://llamalendnft.com/",
-    },
-    { name: "Exit10", logo: "exit10.png", website: "https://exit10.com/" },
-    { name: "Vmex", logo: "vmex.png", website: "https://vmex.finance/" },
-    {
-      name: "Personae Lab",
-      logo: "personaelab.png",
-      website: "https://www.personaelabs.xyz/",
-    },
-    {
-      name: "Privacy & Scaling Explorations",
-      logo: "pse.png",
-      website: "https://pse.dev/en",
-    },
-  ];
+
 
   return (
     <section className="bg-white m-6">
@@ -43,37 +68,6 @@ export default function ClientSection() {
         <h2 className="lg:text-5xl text-3xl font-bold mb-10 sm:text-center">
           Clients
         </h2>
-        {/* <div className="grid grid-cols-3 gap-4">
-          {clients.map((client, index) => (
-            <div
-              key={index}
-              className="bg-gray-100 p-4 col-span-1 rounded relative hover:bg-opacity-5 duration-700"
-            >
-              <img
-                src={client.logo}
-                alt={`${client.name} Logo`}
-                className="mx-left sm:mx-auto ml-4 mt-4 mb-4 w-12 h-12 sm:w-8 sm:h-8 opacity-80"
-              />
-
-              <p className="text-gray-800 text-left sm:text-center font-bold text-lg sm:text-xs ml-4 sm:ml-0 mb-4">
-                {client.name}
-              </p>
-
-              <a
-                href={client.website}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="absolute top-0 right-0 text-blue-500 hover:underline mt-2 mr-2 flex items-center"
-              >
-                <img
-                  src="maximize.png"
-                  alt="External Link"
-                  className="ml-1 w-3 h-3 opacity-50"
-                />
-              </a>
-            </div>
-          ))}
-        </div> */}
         <OrbitingCirclesDemo />
       </div>
     </section>
@@ -82,49 +76,55 @@ export default function ClientSection() {
 
 function OrbitingCirclesDemo() {
   return (
-    <div className="relative flex h-[90vh] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background md:shadow-xl">
-      {/* Inner Circles */}
+    <div className="relative flex h-[100vh] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background md:shadow-xl">
+      {clients.slice(0, 4).map((client, i) => (
       <OrbitingCircles
-        className="size-[30px] border-none bg-transparent"
-        duration={20}
-        delay={20}
-        radius={80}
+        className={"size-[30px] border-none bg-transparent hover:cursor-pointer"}
+        duration={40}
+        delay={10 * i}
+        radius={50}
       >
-        <Icons.gitHub />
+        <a href={client.website} target="_blank">
+            <img src={"./clients/" + client.logo} />
+          </a>
       </OrbitingCircles>
+      ))}
+      {clients.slice(4, 10).map((client, i) => (
       <OrbitingCircles
-        className="size-[30px] border-none bg-transparent"
-        duration={20}
-        delay={10}
-        radius={80}
-      >
-        <Icons.gitHub />
+        className={"size-[45px] border-none bg-transparent hover:cursor-pointer"}
+        duration={60}
+        delay={10 * i}
+        radius={140}
+        >
+          <a href={client.website} target="_blank">
+            <img src={"./clients/" + client.logo} />
+          </a>
       </OrbitingCircles>
-
-      {/* Outer Circles (reverse) */}
+      ))}
+      {clients.slice(10, 17).map((client, i) => (
       <OrbitingCircles
-        className="size-[50px] border-none bg-transparent"
-        radius={250}
-        duration={20}
-        reverse
+        className={"size-[55px] border-none bg-transparent hover:cursor-pointer"}
+        duration={70}
+        delay={10 * i}
+        radius={230}
       >
-        <Icons.gitHub />
+        <a href={client.website} target="_blank">
+            <img src={"./clients/" + client.logo} />
+          </a>
       </OrbitingCircles>
+      ))}
+      {clients.slice(17, 27).map((client, i) => (
       <OrbitingCircles
-        className="size-[50px] border-none bg-transparent"
-        radius={190}
-        duration={20}
-        delay={20}
-        reverse
+        className={"size-[70px] border-none bg-transparent hover:cursor-pointer"}
+        duration={90}
+        delay={10 * i}
+        radius={320}
       >
-        <Icons.gitHub />
+        <a href={client.website} target="_blank">
+            <img src={"./clients/" + client.logo} />
+          </a>
       </OrbitingCircles>
+      ))}
     </div>
   );
 }
-
-const Icons = {
-  gitHub: () => (
-    <img src="./bulb.svg"/>
-  ),
-};
