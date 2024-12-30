0bea7c87dc4179f4476ab75b065bb7845dab9b6f
nullity00 <reshmashaik3011@gmail.com>
2024-12-30 20:21:17 +0400

    fix : build

--- next.config.js
+++ next.config.js
@@ -7,19 +7,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-
-  exportPathMap: async function () {
-    return {
-      "/": { page: "/" },
-      "/contact-us": { page: "/contact-us" },
-      "/fellowships": { page: "/fellowships" },
-      "/members": { page: "/members" },
-      "/services": { page: "/services" },
-      "/zBlock1": { page: "/zBlock1" },
-      "/zBlock2": { page: "/zBlock2" },
-      "/zk": { page: "/zk" },
-    };
-  },
 };
 
 module.exports = nextConfig;
