dead059965ebb27a210e076e636216eabec8befe
nullity00 <reshmashaik3011@gmail.com>
2025-01-01 01:05:21 +0400

    try fix: gtag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,25 +10,30 @@ import Services from "@/modules/Home/3-Services";
 import Fellowships from "@/modules/Home/4-Fellowships";
 import Research from "@/modules/Home/5-Research";
 import { AuditStats } from "@/modules/Home/7-Stats";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function Home() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className="flex flex-col bg-white text-black w-full font-default">
-      {menuOpen ? (
-        <MobileNavbar setMenuOpen={setMenuOpen} />
-      ) : (
-        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      )}
+    <>
+      <GoogleTagManager gtmId="G-NZNG2YQNTY" />
 
-      <HeroSection />
-      <Partners />
-      <Feedback />
-      <AuditStats />
-      <Services />
-      <Research />
-      <Fellowships />
-      <Footer />
-    </div>
+      <div className="flex flex-col bg-white text-black w-full font-default">
+        {menuOpen ? (
+          <MobileNavbar setMenuOpen={setMenuOpen} />
+        ) : (
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        )}
+
+        <HeroSection />
+        <Partners />
+        <Feedback />
+        <AuditStats />
+        <Services />
+        <Research />
+        <Fellowships />
+        <Footer />
+      </div>
+    </>
   );
 }
