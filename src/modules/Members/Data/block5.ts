d448509c80833364513a466a9e41085c59ded70c
nullity00 <reshmashaik3011@gmail.com>
2025-01-14 19:28:55 +0400

    shh

--- src/modules/Members/Data/block5.ts
+++ src/modules/Members/Data/block5.ts
@@ -34,8 +34,8 @@ export const block5 = [
   { name: "HHK", url: "https://github.com/HHK-ETH", promotedToResident: true },
   { name: "Jat", url: "https://github.com/jat9292", promotedToResident: false },
   {
-    name: "ljmanini",
-    url: "https://github.com/lmanini",
+    name: "DrasticWatermelon",
+    twitter: "https://github.com/DrasticWatermelon",
     promotedToResident: true,
   },
   {
